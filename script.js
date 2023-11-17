bd734918821d9c1401a97dc883f5757a7890ff8b
azarma <52371787+azarma-azata@users.noreply.github.com>
2023-11-17 09:32:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.getElementById('pauseButton').addEventListener('click', function() {
     var video = document.querySelector('.video-container video');
     if (video.paused) {
         video.play();
-        this.innerText = '⏸️r';
+        this.innerText = '⏸️';
     } else {
         video.pause();
-        this.innerText = 'Play';
+        this.innerText = '▶️';
     }
 });
